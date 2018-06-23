22c299ad8cc1027919c2a97d12e5a36a91ef54a0
Erik Engi <kireerike@gmail.com>
2018-06-23 18:14:12 +0200

    Context altering improved

--- app/objects.js
+++ app/objects.js
@@ -3,8 +3,9 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-        this.greeting = obj.greeting
-        this.name = obj.name
+        Object.entries(obj).forEach((object) =>
+            this[object[0]] = object[1]
+        )
 
         return fn.call(this)
     },
