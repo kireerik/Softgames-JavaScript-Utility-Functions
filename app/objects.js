5e5768787083dfc64cbf744ddf3cf7f028dbc9c3
Erik Engi <kireerike@gmail.com>
2018-06-23 18:07:31 +0200

    Basic object alter context implemented

--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,10 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
+        this.greeting = obj.greeting
+        this.name = obj.name
 
+        return fn.call(this)
     },
 
     alterObjects : function(constructor, greeting) {
