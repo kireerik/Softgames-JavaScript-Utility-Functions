a810becc06890bc91ad48918499d03ef0bd59ae7
Erik Engi <kireerike@gmail.com>
2018-06-23 17:37:24 +0200

    Basic binary functions implemented

--- app/numbers.js
+++ app/numbers.js
@@ -7,15 +7,15 @@ define(function() {
     },
 
     base10: function(str) {
-
+        return parseInt(str, 2)
     },
 
     convertToBinary: function(num) {
-
+        return ('000000000' + (num >>> 0).toString(2)).substr(-8)
     },
 
     multiply: function(a, b) {
-
+        return Number((a * b).toFixed(8))
     }
   };
 });
