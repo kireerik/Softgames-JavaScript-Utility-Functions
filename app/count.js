0e029bd411d7febcb0eabfee973ff2961407f67c
Erik Engi <kireerike@gmail.com>
2018-06-23 21:39:56 +0200

    Basic counter implemented

--- app/count.js
+++ app/count.js
@@ -2,7 +2,9 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
   return {
-    count : function (start, end) {
+    count : async function (start, end) {
+        for (var i = start; i <= end; i++, await new Promise(resolve => setTimeout(resolve, 100)))
+            console.log(i)
     }
   };
 });
