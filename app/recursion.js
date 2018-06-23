2f1534f1d32b40df0c7af89f6ecf5a76f2d07a56
Erik Engi <kireerike@gmail.com>
2018-06-23 23:02:58 +0200

    Basic recursion implemented

--- app/recursion.js
+++ app/recursion.js
@@ -2,8 +2,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    listFiles: function(data, dirName) {
+    listFiles: function listFiles(data, dirName) {
+        return data.files.reduce((result, item) => {
+            if (typeof item.files != 'undefined')
+                result.push(...listFiles(item))
+            else
+                result.push(item)
 
+            return result
+        }, [])
     },
 
     permute: function(arr) {
