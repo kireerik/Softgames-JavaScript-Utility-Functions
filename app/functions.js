f5b6573650d3701029f988b3061726ce5a86aa4b
Erik Engi <kireerike@gmail.com>
2018-06-23 18:45:49 +0200

    Basic functions implemented

--- app/functions.js
+++ app/functions.js
@@ -3,15 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn(...arr)
     },
 
     speak : function(fn, obj) {
+        Object.entries(obj).forEach((object) =>
+            this[object[0]] = object[1]
+        )
 
+        return fn.call(this)
     },
 
     functionFunction : function(str) {
-
+        return str2 => str + ', ' + str2
     },
 
     makeClosures : function(arr, fn) {
@@ -23,15 +27,25 @@ define(function() {
     },
 
     useArguments : function() {
-
+        return [...arguments].reduce((sum, argument) => sum + argument)
     },
 
-    callIt : function(fn) {
+    callIt : function() {
+        var functionArguments = [...arguments]
+        functionArguments.shift()
 
+        return arguments[0](...functionArguments)
     },
 
-    partialUsingArguments : function(fn) {
+    partialUsingArguments : function() {
+        const mainFunction = arguments[0]
 
+        var functionArguments = [...arguments]
+        functionArguments.shift()
+
+        return function() {
+            return mainFunction(...functionArguments, ...arguments)
+        }
     },
 
     curryIt : function(fn) {
