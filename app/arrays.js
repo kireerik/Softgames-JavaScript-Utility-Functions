6c804e021eb96dfd346ab35be1f759073901e25d
Erik Engi <kireerike@gmail.com>
2018-06-23 16:36:43 +0200

    Basic array functions implemented

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+        return arr.indexOf(item)
     },
 
     sum : function(arr) {
-
+        return arr.reduce((sum, value) => sum + value)
     },
 
     remove : function(arr, item) {
+        while (arr.indexOf(item) != -1)
+            arr.splice(arr.indexOf(item), 1)
 
+        return arr
     },
 
     removeWithoutCopy : function(arr, item) {
+        while (arr.indexOf(item) != -1)
+            arr.splice(arr.indexOf(item), 1)
 
+        return arr
     },
 
     append : function(arr, item) {
