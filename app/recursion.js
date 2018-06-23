fe3856ff144243b2701996026b3cf706b795acac
Erik Engi <kireerike@gmail.com>
2018-06-23 23:23:19 +0200

    Basic subdir listing implemented

--- app/recursion.js
+++ app/recursion.js
@@ -3,10 +3,12 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function listFiles(data, dirName) {
+        const isSearchedDirecory = data.dir == dirName
+
         return data.files.reduce((result, item) => {
             if (typeof item.files != 'undefined')
-                result.push(...listFiles(item))
-            else
+                result.push(...listFiles(item, isSearchedDirecory ? '' : dirName))
+            else if (isSearchedDirecory || !dirName)
                 result.push(item)
 
             return result
