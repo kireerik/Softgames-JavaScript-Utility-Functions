6ca3b2b4039cbbf545a7784187e9569e2af2f91d
Erik Engi <kireerike@gmail.com>
2018-06-24 00:49:04 +0200

    Basic permutation function implemented

--- app/recursion.js
+++ app/recursion.js
@@ -16,7 +16,30 @@ define(function() {
     },
 
     permute: function(arr) {
+        Array.prototype.swap = function (index, otherIndex) {
+            var valueAtIndex = this[index]
 
+            this[index] = this[otherIndex]
+            this[otherIndex] = valueAtIndex
+        }
+
+        const length = arr.length
+        var result = [arr.slice()]
+
+        for (let i = 1, heap = new Array(length).fill(0)
+            ; i < length
+        ;)
+            if (heap[i] < i) {
+                arr.swap(i, i % 2 && heap[i])
+                result.push(arr.slice())
+                heap[i]++
+                i = 1
+            } else {
+                heap[i] = 0
+                i++
+            }
+
+        return result
     }
   };
 });
