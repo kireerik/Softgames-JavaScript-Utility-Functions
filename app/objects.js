1bd433f7f5b10d3d37a93affe922377a1069b0fd
Erik Engi <kireerike@gmail.com>
2018-06-24 15:22:48 +0200

    Object functions implemented

--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-        Object.entries(obj).forEach((object) =>
+        Object.entries(obj).forEach(object =>
             this[object[0]] = object[1]
         )
 
@@ -11,11 +11,13 @@ define(function() {
     },
 
     alterObjects : function(constructor, greeting) {
-
+        constructor.prototype.greeting = greeting
     },
 
     iterate : function(obj) {
-
+        return Object.entries(obj).map(object =>
+            object[0] + ': ' + object[1]
+        )
     }
   };
 });
