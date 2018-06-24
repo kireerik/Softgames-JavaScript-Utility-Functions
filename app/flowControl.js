8e362810d8e97d71bb3f885b7ab53d24e5d72bf1
Erik Engi <kireerike@gmail.com>
2018-06-24 16:08:54 +0200

    Flow control implemented

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,6 +11,12 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      const result = Number.isInteger(num) ?
+        (!(num % 3) ? 'fizz' : '') + (!(num % 5) ? 'buzz' : '')
+      :
+        false
+
+      return result !== '' ? result : num
     }
   };
 });
